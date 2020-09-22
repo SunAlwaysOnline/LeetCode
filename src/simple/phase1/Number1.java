a628ec1940117bef1593005ffda8469b6895f2a3
SunAlwaysOnline <767638734@qq.com>
2020-09-22 17:03:35 +0800

    add:提交第1题

---
+++ src/simple/phase1/Number1.java
@@ -0,0 +1,50 @@
+package simple.phase1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author qcy
+ * @create 2020/09/22 17:02:04
+ * 1. 两数之和
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * <p>
+ *  
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ */
+public class Number1 {
+
+    //两层for循环
+    public int[] twoSum1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    //使用HashMap
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int p = target - nums[i];
+            if (map.containsKey(p)) {
+                return new int[]{i, map.get(p)};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+    
+}
