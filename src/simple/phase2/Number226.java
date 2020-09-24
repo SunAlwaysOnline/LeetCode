fc20d0374d8085daab4ff2274f5f94b3c3cc3529
SunAlwaysOnline <767638734@qq.com>
2020-09-24 16:58:30 +0800

    add:提交第226题

---
+++ src/simple/phase2/Number226.java
@@ -0,0 +1,58 @@
+package simple.phase2;
+
+/**
+ * @author qcy
+ * @create 2020/09/24 15:14:31
+ * 226. 翻转二叉树
+ * 翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ * 备注:
+ * 这个问题是受到 Max Howell 的 原问题 启发的 ：
+ *
+ * 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+ */
+public class Number226 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        //交换root节点的左右子树
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        //翻转left节点的左右子树
+        invertTree(root.left);
+        //翻转right节点的左右子树
+        invertTree(root.right);
+
+        return root;
+    }
+
+}
