17ba6a1f1264d04570d8745bc1c063b15702490f
SunAlwaysOnline <767638734@qq.com>
2020-09-27 19:45:42 +0800

    add:提交第200题

---
+++ src/medium/phase1/Number200.java
@@ -0,0 +1,99 @@
+package medium.phase1;
+
+import method.DFS;
+
+/**
+ * @author qcy
+ * @create 2020/09/27 17:48:31
+ * 200. 岛屿数量
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ *
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向或竖直方向上相邻的陆地连接形成。
+ *
+ * 此外，你可以假设该网格的四条边均被水包围。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * [
+ * ['1','1','1','1','0'],
+ * ['1','1','0','1','0'],
+ * ['1','1','0','0','0'],
+ * ['0','0','0','0','0']
+ * ]
+ * 输出: 1
+ * 示例 2:
+ *
+ * 输入:
+ * [
+ * ['1','1','0','0','0'],
+ * ['1','1','0','0','0'],
+ * ['0','0','1','0','0'],
+ * ['0','0','0','1','1']
+ * ]
+ * 输出: 3
+ * 解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+ *
+ */
+public class Number200 implements DFS {
+
+    //              (m-1,n)
+    //      (m,n-1) (m, n ) (m,n+1)
+    //              (m+1,n)
+    char[][] map;
+
+    public int numIslands(char[][] grid) {
+        map = grid;
+        int sum = 0;
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] == '1') {
+                    sum++;
+                    dfs(i, j);
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    private void dfs(int m, int n) {
+        //如果不在区域中
+        if (!inArea(m, n)) {
+            return;
+        }
+        //如果是水
+        if (map[m][n] == '0') {
+            return;
+        }
+        //如果已经遍历过
+        if (map[m][n] == '2') {
+            return;
+        }
+        //当前位置为陆地，那么设置为访问过
+        map[m][n] = '2';
+        //一直向上访问
+        dfs(m - 1, n);
+        //一直向下访问
+        dfs(m + 1, n);
+        //一直向左访问
+        dfs(m, n - 1);
+        //一直向右访问
+        dfs(m, n + 1);
+    }
+
+    //判断是否在网格中
+    private boolean inArea(int m, int n) {
+        return m >= 0 && m < map.length
+                && n >= 0 && n < map[0].length;
+    }
+
+    public static void main(String[] args) {
+        char[][] map = {{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '0', '0', '0'}};
+        int i = new Number200().numIslands(map);
+        System.out.println(i);
+    }
+}
---
+++ src/simple/phase2/Number392.java
@@ -0,0 +1,54 @@
+package simple.phase2;
+
+/**
+ * @author qcy
+ * @create 2020/09/27 15:16:09
+ */
+public class Number392 {
+    //s: ace   t: babcde
+
+    public boolean isSubsequence(String s, String t) {
+        int sLength = s.length();
+        int tLength = t.length();
+
+        int sIndex = 0;
+        int tIndex = 0;
+
+        while (sIndex < sLength && tIndex < tLength) {
+            if (s.charAt(sIndex) == t.charAt(tIndex)) {
+                sIndex++;
+            }
+            tIndex++;
+        }
+
+        return sIndex == s.length();
+    }
+
+    public boolean isSubsequence2(String s, String t) {
+        // 预处理
+        t = " " + t; // 开头加一个空字符作为匹配入口
+        int n = t.length();
+        int[][] dp = new int[n][26]; // 记录每个位置的下一个ch的位置
+        for (char ch = 0; ch < 26; ch++) {
+            int p = -1;
+            for (int i = n - 1; i >= 0; i--) { // 从后往前记录dp
+                dp[i][ch] = p;
+                if (t.charAt(i) == ch + 'a') p = i;
+            }
+        }
+        // 匹配
+        int i = 0;
+        for (char ch : s.toCharArray()) { // 跳跃遍历
+            i = dp[i][ch - 'a'];
+            if (i == -1) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String t = "abcde";
+        String s = "ace";
+        System.out.println(new Number392().isSubsequence2(s, t));
+    }
+
+}
