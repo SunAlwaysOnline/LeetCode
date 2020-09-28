4c383ee06a70650af859c072194bfc393ea39247
SunAlwaysOnline <767638734@qq.com>
2020-09-28 17:28:41 +0800

    add:提交leetcode21与offer25题的框架,待写

---
+++ src/jzoffer/Offer25.java
@@ -0,0 +1,22 @@
+package jzoffer;
+
+/**
+ * @author qcy
+ * @create 2020/09/28 17:15:46
+ */
+public class Offer25 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        return null;
+    }
+
+}
---
+++ src/leetcode/simple/phase2/Number21.java
@@ -0,0 +1,29 @@
+package leetcode.simple.phase2;
+
+/**
+ * @author qcy
+ * @create 2020/09/28 17:17:48
+ */
+public class Number21 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
