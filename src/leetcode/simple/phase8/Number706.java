74c4b2e54f99913a6e93481349f5076ce195b81a
SunAlwaysOnline <767638734@qq.com>
2020-10-08 23:21:58 +0800

    fix:修改第706题

--- src/leetcode/simple/phase8/Number706.java
+++ src/leetcode/simple/phase8/Number706.java
@@ -4,17 +4,17 @@ package leetcode.simple.phase8;
  * @author qcy
  * @create 2020/10/08 21:15:10
  * 706. 设计哈希映射
- *
+ * <p>
  * 不使用任何内建的哈希表库设计一个哈希映射
- *
+ * <p>
  * 具体地说，你的设计应该包含以下的功能
- *
+ * <p>
  * put(key, value)：向哈希映射中插入(键,值)的数值对。如果键对应的值已经存在，更新这个值。
  * get(key)：返回给定的键所对应的值，如果映射中不包含这个键，返回-1。
  * remove(key)：如果映射中存在这个键，删除这个数值对。
- *
+ * <p>
  * 示例：
- *
+ * <p>
  * MyHashMap hashMap = new MyHashMap();
  * hashMap.put(1, 1);          
  * hashMap.put(2, 2);        
@@ -24,13 +24,12 @@ package leetcode.simple.phase8;
  * hashMap.get(2);            // 返回 1
  * hashMap.remove(2);         // 删除键为2的数据
  * hashMap.get(2);            // 返回 -1 (未找到)
- *
+ * <p>
  * 注意：
- *
+ * <p>
  * 所有的值都在 [0, 1000000]的范围内。
  * 操作的总数目在[1, 10000]范围内。
  * 不要使用内建的哈希库。
- *
  */
 
 public class Number706 {
@@ -140,7 +139,9 @@ public class Number706 {
                 }
                 if (nextNode.key == key) {
                     node.next = nextNode.next;
+                    nextNode.next = null;
                     nextNode = null;
+                    return;
                 }
                 node = node.next;
             }
