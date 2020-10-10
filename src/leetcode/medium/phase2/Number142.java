4ae9ba3ee656189e86e6cb35e6b0835900f11504
SunAlwaysOnline <767638734@qq.com>
2020-10-10 15:09:28 +0800

    add:提交第142题

---
+++ src/leetcode/medium/phase2/Number142.java
@@ -0,0 +1,74 @@
+package leetcode.medium.phase2;
+
+
+import method.FastAndSlowPointer;
+
+/**
+ * @author qcy
+ * @create 2020/10/10 14:47:56
+ * 142. 环形链表 II
+ * <p>
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ * <p>
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ * <p>
+ * 说明：不允许修改给定的链表。
+ */
+public class Number142 implements FastAndSlowPointer {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean hasCycle = false;
+
+        while (fast != null && fast.next != null && slow != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                hasCycle = true;
+                break;
+            }
+        }
+
+        if (hasCycle) {
+            fast = head;
+            while (fast != slow) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            return slow;
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = node3;
+
+        System.out.println(new Number142().detectCycle(node1).val);
+    }
+}
