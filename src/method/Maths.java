33dccf77c6fa05946e09b4729b5267fed6e61009
SunAlwaysOnline <767638734@qq.com>
2020-10-17 21:01:54 +0800

    add:提交第268题

---
+++ src/method/Maths.java
@@ -0,0 +1,14 @@
+package method;
+
+import java.lang.annotation.*;
+
+/**
+ * @author qcy
+ * @create 2020/10/17 21:00:20
+ * 数学解法
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Maths {
+}
