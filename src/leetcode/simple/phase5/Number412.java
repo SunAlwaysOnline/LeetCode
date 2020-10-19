c7f2df41089866faa07d07c4e8852e97f49d2c61
SunAlwaysOnline <767638734@qq.com>
2020-10-19 16:33:49 +0800

    add:提交第412题

---
+++ src/leetcode/simple/phase5/Number412.java
@@ -0,0 +1,70 @@
+package leetcode.simple.phase5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author qcy
+ * @create 2020/10/19 16:16:28
+ * <p>
+ * 412. Fizz Buzz
+ * <p>
+ * 写一个程序，输出从 1 到 n 数字的字符串表示。
+ * <p>
+ * 1. 如果 n 是3的倍数，输出“Fizz”；
+ * <p>
+ * 2. 如果 n 是5的倍数，输出“Buzz”；
+ * <p>
+ * 3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+ * <p>
+ * 示例：
+ * <p>
+ * n = 15,
+ * <p>
+ * 返回:
+ * [
+ * "1",
+ * "2",
+ * "Fizz",
+ * "4",
+ * "Buzz",
+ * "Fizz",
+ * "7",
+ * "8",
+ * "Fizz",
+ * "Buzz",
+ * "11",
+ * "Fizz",
+ * "13",
+ * "14",
+ * "FizzBuzz"
+ * ]
+ */
+
+public class Number412 {
+
+    public List<String> fizzBuzz(int n) {
+
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) {
+                result.add("FizzBuzz");
+                continue;
+            }
+            if (i % 3 == 0) {
+                result.add("Fizz");
+                continue;
+            }
+            if (i % 5 == 0) {
+                result.add("Buzz");
+                continue;
+            }
+            result.add(String.valueOf(i));
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        new Number412().fizzBuzz(15).stream().forEach(System.out::println);
+    }
+}
