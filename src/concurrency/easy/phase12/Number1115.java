9ef110da0fe23a7b38dc0240e76d8072873cd9a6
SunAlwaysOnline <767638734@qq.com>
2020-11-05 16:42:18 +0800

    add:提交concurrency第1115题

---
+++ src/concurrency/easy/phase12/Number1115.java
@@ -0,0 +1,102 @@
+package concurrency.easy.phase12;
+
+/**
+ * @author qcy
+ * @create 2020/11/05 16:31:33
+ * 1115. 交替打印FooBar
+ *
+ * 我们提供一个类：
+ *
+ * class FooBar {
+ *   public void foo() {
+ *     for (int i = 0; i < n; i++) {
+ *       print("foo");
+ *     }
+ *   }
+ *
+ *   public void bar() {
+ *     for (int i = 0; i < n; i++) {
+ *       print("bar");
+ *     }
+ *   }
+ * }
+ * 两个不同的线程将会共用一个 FooBar 实例。其中一个线程将会调用 foo() 方法，另一个线程将会调用 bar() 方法。
+ *
+ * 请设计修改程序，以确保 "foobar" 被输出 n 次。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: n = 1
+ * 输出: "foobar"
+ * 解释: 这里有两个线程被异步启动。其中一个调用 foo() 方法, 另一个调用 bar() 方法，"foobar" 将被输出一次。
+ * 示例 2:
+ *
+ * 输入: n = 2
+ * 输出: "foobarfoobar"
+ * 解释: "foobar" 将被输出两次。
+ *
+ */
+public class Number1115 {
+
+    static class FooBar {
+        private int n;
+        private volatile boolean flag = true;
+
+        public FooBar(int n) {
+            this.n = n;
+        }
+
+        public void foo(Runnable printFoo) throws InterruptedException {
+
+            for (int i = 0; i < n; i++) {
+                synchronized (this) {
+                    while (!flag) {
+                        this.wait();
+                    }
+                    printFoo.run();
+                    flag = false;
+                    this.notify();
+                }
+            }
+        }
+
+        public void bar(Runnable printBar) throws InterruptedException {
+
+            for (int i = 0; i < n; i++) {
+                synchronized (this) {
+                    while (flag) {
+                        this.wait();
+                    }
+                    printBar.run();
+                    flag = true;
+                    this.notify();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        FooBar fooBar = new FooBar(5);
+        new Thread(() -> {
+            try {
+                fooBar.bar(() -> {
+                    System.out.print("bar");
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                fooBar.foo(() -> {
+                    System.out.print("foo");
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+    }
+}
