ae8ec2140a2f1b5e4c3b25b52dfac5ddfebca6e7
SunAlwaysOnline <767638734@qq.com>
2020-11-11 10:38:27 +0800

    add:提交leetcode第35题

---
+++ src/leetcode/easy/phase1/Number35.java
@@ -0,0 +1,79 @@
+package leetcode.easy.phase1;
+
+import complexity.SpaceComplexity;
+import complexity.TimeComplexity;
+import method.BinarySearch;
+import method.Optimal;
+import method.Orignal;
+
+/**
+ * @author qcy
+ * @create 2020/11/11 09:14:49
+ * 35. 搜索插入位置
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ *
+ * 你可以假设数组中无重复元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ * 示例 3:
+ *
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ * 示例 4:
+ *
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ *
+ */
+public class Number35 {
+
+    @Orignal
+    @TimeComplexity("O(n)")
+    @SpaceComplexity("O(1)")
+    public int searchInsert1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+            if (nums[i] > target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    @BinarySearch
+    @Optimal
+    @TimeComplexity("O(logn)")
+    @SpaceComplexity("O(1)")
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            }
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3};
+        System.out.println(new Number35().searchInsert(nums, 2));
+    }
+
+}
---
+++ src/method/BinarySearch.java
@@ -0,0 +1,14 @@
+package method;
+
+import java.lang.annotation.*;
+
+/**
+ * @author qcy
+ * @create 2020/11/11 09:32:36
+ * 二分查找
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface BinarySearch {
+}
