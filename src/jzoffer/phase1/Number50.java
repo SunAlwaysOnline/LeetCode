be6d98820a3197845b708a90dc2c9613a124f915
戚春阳 <qichunyang@beyondcent.com>
2020-11-27 16:00:24 +0800

    add:提交jzoffer第50题

---
+++ src/jzoffer/phase1/Number50.java
@@ -0,0 +1,58 @@
+package jzoffer.phase1;
+
+import complexity.SpaceComplexity;
+import complexity.TimeComplexity;
+import method.Orignal;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author qcy
+ * @create 2020/11/27 15:32:05
+ * 剑指 Offer 50. 第一个只出现一次的字符
+ *
+ * 在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+ *
+ * 示例:
+ *
+ * s = "abaccdeff"
+ * 返回 "b"
+ *
+ * s = ""
+ * 返回 " "
+ *  
+ *
+ * 限制：
+ *
+ * 0 <= s 的长度 <= 50000
+ *
+ */
+public class Number50 {
+
+    @Orignal
+    @method.Map
+    //map的长度不随着字符串的长度增加而增加o
+    @SpaceComplexity("O(1)")
+    @TimeComplexity("O(n)")
+    public char firstUniqChar(String s) {
+        char[] chars = s.toCharArray();
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        for (char c : chars) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return ' ';
+    }
+
+    public static void main(String[] args) {
+        String s = "cc";
+        System.out.println(new Number50().firstUniqChar(s));
+    }
+
+}
---
+++ src/jzoffer/phase2/Main.java
@@ -0,0 +1,8 @@
+package jzoffer.phase2;
+
+/**
+ * @author qcy
+ * @create 2020/11/27 15:31:48
+ */
+public class Main {
+}
