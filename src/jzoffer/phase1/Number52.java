20ed9a48df92859b4502155096939ae6d2052fc7
SunAlwaysOnline <767638734@qq.com>
2020-11-30 16:15:49 +0800

    add:提交jzoffer第52题

---
+++ src/jzoffer/phase1/Number52.java
@@ -0,0 +1,83 @@
+package jzoffer.phase1;
+
+import method.DoublePointer;
+
+/**
+ * @author qcy
+ * @create 2020/11/30 15:54:54
+ * 剑指 Offer 52. 两个链表的第一个公共节点
+ *
+ * 输入两个链表，找出它们的第一个公共节点。
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+ * 输出：Reference of the node with value = 8
+ * 输入解释：相交节点的值为 8 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ *  
+ *
+ * 示例 2：
+ *
+ * 输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+ * 输出：null
+ * 输入解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+ * 解释：这两个链表不相交，因此返回 null。
+ *  
+ *
+ * 注意：
+ *
+ * 如果两个链表没有交点，返回 null.
+ * 在返回结果后，两个链表仍须保持原有的结构。
+ * 可假定整个链表结构中没有循环。
+ * 程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+ * 本题与主站 160 题相同：https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+ *
+ */
+public class Number52 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    @DoublePointer
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (true) {
+            //第一种情况,pA!=pB,说明没相遇,则继续往下走
+            //第二种情况,pA==pB,且它们都不为null,则说明相交，交点即为pA
+            //第三种情况,pA==pB,且它们都为null,则说明在尾部相遇,没有交点,返回pA,此时pA也为null
+            if (pA == pB) {
+                return pA;
+            }
+
+            if (pA == null) {
+                pA = headB;
+            } else {
+                pA = pA.next;
+            }
+
+            if (pB == null) {
+                pB = headA;
+            } else {
+                pB = pB.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Number52().getIntersectionNode(new ListNode(1), new ListNode(2));
+    }
+
+}
