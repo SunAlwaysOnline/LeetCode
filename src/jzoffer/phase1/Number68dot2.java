b55bd3e048c3412866b5d8d68af610045818ce65
SunAlwaysOnline <767638734@qq.com>
2020-12-02 09:39:41 +0800

    add:提交jzoffer第9题

---
+++ src/jzoffer/phase1/Number68dot2.java
@@ -0,0 +1,23 @@
+//package jzoffer.phase1;
+//
+///**
+// * @author qcy
+// * @create 2020/12/01 14:51:21
+// */
+//public class Number68dot2 {
+//
+//    public class TreeNode {
+//        int val;
+//        TreeNode left;
+//        TreeNode right;
+//
+//        TreeNode(int x) {
+//            val = x;
+//        }
+//    }
+//
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//
+//    }
+//
+//}
---
+++ src/jzoffer/phase1/Number9.java
@@ -0,0 +1,68 @@
+package jzoffer.phase1;
+
+import java.util.Stack;
+
+/**
+ * @author qcy
+ * @create 2020/12/02 09:31:01
+ * 剑指 Offer 09. 用两个栈实现队列
+ *
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：
+ * ["CQueue","appendTail","deleteHead","deleteHead"]
+ * [[],[3],[],[]]
+ * 输出：[null,null,3,-1]
+ * 示例 2：
+ *
+ * 输入：
+ * ["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+ * [[],[],[5],[2],[],[]]
+ * 输出：[null,-1,null,null,5,2]
+ * 提示：
+ *
+ * 1 <= values <= 10000
+ * 最多会对 appendTail、deleteHead 进行 10000 次调用
+ *
+ */
+public class Number9 {
+    static class CQueue {
+
+        private Stack<Integer> stack1;
+        private Stack<Integer> stack2;
+
+        public CQueue() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
+        }
+
+        public void appendTail(int value) {
+            stack1.push(value);
+        }
+
+        public int deleteHead() {
+            if (stack2.isEmpty()) {
+                while (!stack1.isEmpty()) {
+                    stack2.push(stack1.pop());
+                }
+                if (stack2.isEmpty()) {
+                    return -1;
+                }
+            }
+            return stack2.pop();
+        }
+    }
+
+    public static void main(String[] args) {
+        CQueue queue = new CQueue();
+        queue.appendTail(1);
+        queue.appendTail(2);
+        System.out.println(queue.deleteHead());
+        System.out.println(queue.deleteHead());
+        System.out.println(queue.deleteHead());
+    }
+}
