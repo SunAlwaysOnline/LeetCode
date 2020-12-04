0aeff797374e6fc2f7d9ca334c337b251fe9f2f8
SunAlwaysOnline <767638734@qq.com>
2020-12-04 17:57:59 +0800

    add:提交leetcode第572题框架,待完成

---
+++ src/leetcode/easy/phase6/Number572.java
@@ -0,0 +1,34 @@
+//package leetcode.easy.phase6;
+//
+///**
+// * @author qcy
+// * @create 2020/12/04 17:28:45
+// */
+//public class Number572 {
+//
+//    public class TreeNode {
+//        int val;
+//        TreeNode left;
+//        TreeNode right;
+//
+//        TreeNode() {
+//        }
+//
+//        TreeNode(int val) {
+//            this.val = val;
+//        }
+//
+//        TreeNode(int val, TreeNode left, TreeNode right) {
+//            this.val = val;
+//            this.left = left;
+//            this.right = right;
+//        }
+//    }
+//
+//    public boolean isSubtree(TreeNode s, TreeNode t) {
+//
+//    }
+//
+//
+//
+//}
