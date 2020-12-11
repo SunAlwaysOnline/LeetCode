f102da99595d40f24915b56628d057559e944a56
SunAlwaysOnline <767638734@qq.com>
2020-12-11 15:00:50 +0800

    add:提交leetcode(√)|jzoffer()|interview()第199题

---
+++ src/leetcode/medium/phase1/Number199.java
@@ -0,0 +1,75 @@
+package leetcode.medium.phase1;
+
+import complexity.SpaceComplexity;
+import complexity.TimeComplexity;
+import method.Iteration;
+import method.Optimal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author qcy
+ * @create 2020/12/11 14:41:07
+ * 199. 二叉树的右视图
+ *
+ * 给定一棵二叉树，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ *
+ * 示例:
+ *
+ * 输入: [1,2,3,null,5,null,4]
+ * 输出: [1, 3, 4]
+ * 解释:
+ *
+ *    1            <---
+ *  /   \
+ * 2     3         <---
+ *  \     \
+ *   5     4       <---
+ *
+ */
+public class Number199 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    @Iteration
+    @Optimal
+    @SpaceComplexity("O(n)")
+    @TimeComplexity("O(n)")
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int length = queue.size();
+            for (int i = 0; i < length; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                if (i == length - 1) {
+                    list.add(node.val);
+                }
+            }
+        }
+        return list;
+    }
+
+}
