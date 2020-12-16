49b0333cada072f6560ec796d9ad6f000978e426
SunAlwaysOnline <767638734@qq.com>
2020-12-16 17:39:30 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()第563题

---
+++ src/datastructure/BinaryTree.java
@@ -0,0 +1,14 @@
+package datastructure;
+
+import java.lang.annotation.*;
+
+/**
+ * @author qcy
+ * @create 2020/12/16 17:37:48
+ * 二叉树
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface BinaryTree {
+}
