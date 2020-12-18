36e03b2ffa38e5da9238a6e7a82e7799bad16a53
SunAlwaysOnline <767638734@qq.com>
2020-12-18 11:42:50 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()第129题

---
+++ src/leetcode/medium/phase2/Number129.java
@@ -0,0 +1,103 @@
+package leetcode.medium.phase2;
+
+import complexity.SpaceComplexity;
+import complexity.TimeComplexity;
+import datastructure.BinaryTree;
+import method.*;
+
+/**
+ * @author qcy
+ * @create 2020/12/18 10:39:59
+ * 129. 求根到叶子节点数字之和
+ *
+ * 给定一个二叉树，它的每个结点都存放一个 0-9 的数字，每条从根到叶子节点的路径都代表一个数字。
+ *
+ * 例如，从根到叶子节点路径 1->2->3 代表数字 123。
+ *
+ * 计算从根到叶子节点生成的所有数字之和。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3]
+ *     1
+ *    / \
+ *   2   3
+ * 输出: 25
+ * 解释:
+ * 从根到叶子节点路径 1->2 代表数字 12.
+ * 从根到叶子节点路径 1->3 代表数字 13.
+ * 因此，数字总和 = 12 + 13 = 25.
+ * 示例 2:
+ *
+ * 输入: [4,9,0,5,1]
+ *     4
+ *    / \
+ *   9   0
+ *  / \
+ * 5   1
+ * 输出: 1026
+ * 解释:
+ * 从根到叶子节点路径 4->9->5 代表数字 495.
+ * 从根到叶子节点路径 4->9->1 代表数字 491.
+ * 从根到叶子节点路径 4->0 代表数字 40.
+ * 因此，数字总和 = 495 + 491 + 40 = 1026.
+ *
+ */
+@Classic
+@BinaryTree
+public class Number129 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    int res = 0;
+    int cur = 0;
+
+    @Recursion
+    @Recall
+    @DFS
+    @SpaceComplexity("O(h)")
+    @TimeComplexity("O(n)")
+    @Optimal
+    public int sumNumbers(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    private void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        cur = cur * 10 + node.val;
+        if (node.left == null && node.right == null) {
+            res += cur;
+        }
+        dfs(node.left);
+        dfs(node.right);
+        cur /= 10;
+    }
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+
+        n1.left = n2;
+        n1.right = n3;
+
+        n2.right = n4;
+
+        System.out.println(new Number129().sumNumbers(n1));
+    }
+
+}
