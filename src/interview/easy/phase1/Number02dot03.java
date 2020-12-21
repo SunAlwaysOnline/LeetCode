2564c657f143a8fb0538195dd454e272913e2d47
SunAlwaysOnline <767638734@qq.com>
2020-12-21 16:21:10 +0800

    add:提交leetcode()|jzoffer()|interview(√)|lcp()第02.03题

---
+++ src/interview/easy/phase1/Number02dot03.java
@@ -0,0 +1,58 @@
+package interview.easy.phase1;
+
+import datastructure.LinkedList;
+
+/**
+ * @author qcy
+ * @create 2020/12/21 15:52:58
+ * 面试题 02.03. 删除中间节点
+ *
+ * 实现一种算法，删除单向链表中间的某个节点（即不是第一个或最后一个节点），假定你只能访问该节点。
+ * 
+ *
+ * 示例：
+ *
+ * 输入：单向链表a->b->c->d->e->f中的节点c
+ * 结果：不返回任何数据，但该链表变为a->b->d->e->f
+ *
+ */
+@LinkedList
+public class Number02dot03 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        if (node == null) {
+            return;
+        }
+
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(4);
+        ListNode n2 = new ListNode(5);
+        ListNode n3 = new ListNode(1);
+        ListNode n4 = new ListNode(9);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+
+        new Number02dot03().deleteNode(n3);
+
+        while (n1 != null) {
+            System.out.println(n1.val);
+            n1 = n1.next;
+        }
+    }
+
+}
