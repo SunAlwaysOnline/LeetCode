5f4b089a46a60638add1907eb48fdb64f50c513a
SunAlwaysOnline <767638734@qq.com>
2020-12-23 15:09:09 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()第622题

---
+++ src/datastructure/Array.java
@@ -0,0 +1,14 @@
+package datastructure;
+
+import java.lang.annotation.*;
+
+/**
+ * @author qcy
+ * @create 2020/12/23 15:07:58
+ * 数组
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Array {
+}
