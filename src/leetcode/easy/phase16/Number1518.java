faed543acea9f1db0f71e587cbf5d1a1e5a94eed
SunAlwaysOnline <767638734@qq.com>
2020-12-28 15:02:27 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()第1518题

---
+++ src/leetcode/easy/phase16/Number1518.java
@@ -0,0 +1,63 @@
+package leetcode.easy.phase16;
+
+/**
+ * @author qcy
+ * @create 2020/12/28 14:53:10
+ * 1518. 换酒问题
+ *
+ * 小区便利店正在促销，用 numExchange 个空酒瓶可以兑换一瓶新酒。你购入了 numBottles 瓶酒。
+ *
+ * 如果喝掉了酒瓶中的酒，那么酒瓶就会变成空的。
+ *
+ * 请你计算 最多 能喝到多少瓶酒。
+ * 
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：numBottles = 9, numExchange = 3
+ * 输出：13
+ * 解释：你可以用 3 个空酒瓶兑换 1 瓶酒。
+ * 所以最多能喝到 9 + 3 + 1 = 13 瓶酒。
+ * 示例 2：
+ *
+ *
+ * 输入：numBottles = 15, numExchange = 4
+ * 输出：19
+ * 解释：你可以用 4 个空酒瓶兑换 1 瓶酒。
+ * 所以最多能喝到 15 + 3 + 1 = 19 瓶酒。
+ * 示例 3：
+ *
+ * 输入：numBottles = 5, numExchange = 5
+ * 输出：6
+ * 示例 4：
+ *
+ * 输入：numBottles = 2, numExchange = 3
+ * 输出：2
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= numBottles <= 100
+ * 2 <= numExchange <= 100
+ *
+ */
+public class Number1518 {
+
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int count = numBottles;
+        while (numBottles >= numExchange) {
+            int divided = numBottles / numExchange;
+            count += divided;
+            numBottles = numBottles - divided * numExchange + divided;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int numBottles = 15;
+        int numExchange = 4;
+        System.out.println(new Number1518().numWaterBottles(numBottles, numExchange));
+    }
+
+}
