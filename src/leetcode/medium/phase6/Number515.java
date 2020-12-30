21101aa832a44a43ffcd8cfcf225153826d70668
SunAlwaysOnline <767638734@qq.com>
2020-12-30 14:47:55 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()第515题

---
+++ src/leetcode/medium/phase6/Number515.java
@@ -0,0 +1,84 @@
+package leetcode.medium.phase6;
+
+import datastructure.BinaryTree;
+import method.BFS;
+import method.Classic;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author qcy
+ * @create 2020/12/30 14:44:23
+ * 515. 在每个树行中找最大值
+ *
+ * 您需要在二叉树的每一行中找到最大的值。
+ *
+ * 示例：
+ *
+ * 输入:
+ *
+ *           1
+ *          / \
+ *         3   2
+ *        / \   \
+ *       5   3   9
+ *
+ * 输出: [1, 3, 9]
+ *
+ */
+@BinaryTree
+@Classic
+public class Number515 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    @BFS
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                max = Math.max(max, node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            list.add(max);
+        }
+        return list;
+    }
+
+}
