ba52bfad3599dbd3d7d563fe6f094a90ecf88ac2
SunAlwaysOnline <767638734@qq.com>
2021-01-02 21:06:06 +0800

    add:提交leetcode()|jzoffer(√)|interview()|lcp()第18题

---
+++ src/interview/easy/phase16/Number16dot07.java
@@ -0,0 +1,16 @@
+package interview.easy.phase16;
+
+import method.WaitForComplete;
+
+/**
+ * @author qcy
+ * @create 2021/01/02 20:51:31
+ */
+@WaitForComplete
+public class Number16dot07 {
+
+    public int maximum(int a, int b) {
+        return -1;
+    }
+
+}
--- src/method/WaitForComplete.java
+++ src/method/WaitForComplete.java
@@ -4,11 +4,10 @@ import java.lang.annotation.*;
 
 /**
  * @author qcy
- * @create 2020/12/14 11:02:02
- * 还有更加优化的做法
+ * @create 2021/01/02 20:53:05
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface WaitForOptimize {
+public @interface WaitForComplete {
 }
