6f22333e13194d6c98774673d485bdc6078f2dd2
SunAlwaysOnline <767638734@qq.com>
2021-01-06 09:34:53 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()第409题

---
+++ src/leetcode/easy/phase5/Number409.java
@@ -0,0 +1,61 @@
+package leetcode.easy.phase5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author qcy
+ * @create 2021/01/06 09:13:07
+ * 409. 最长回文串
+ *
+ * 给定一个包含大写字母和小写字母的字符串，找到通过这些字母构造成的最长的回文串。
+ *
+ * 在构造过程中，请注意区分大小写。比如 "Aa" 不能当做一个回文字符串。
+ *
+ * 注意:
+ * 假设字符串的长度不会超过 1010。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * "abccccdd"
+ *
+ * 输出:
+ * 7
+ *
+ * 解释:
+ * 我们可以构造的最长的回文串是"dccaccd", 它的长度是 7。
+ *
+ */
+public class Number409 {
+
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        boolean containOne = false;
+        int sum = 0;
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            int val = entry.getValue();
+            if ((val & 1) == 0) {
+                sum += val;
+            } else {
+                sum += val - 1;
+                containOne = true;
+            }
+        }
+        if (containOne) {
+            sum += 1;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        String s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+        System.out.println(new Number409().longestPalindrome(s));
+    }
+
+}
