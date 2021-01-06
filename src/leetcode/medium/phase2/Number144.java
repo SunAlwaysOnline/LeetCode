9c1d530f0e2084f3963a5d593eb5ecb2cc9ba7ff
SunAlwaysOnline <767638734@qq.com>
2021-01-06 20:21:27 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()第144题

---
+++ src/leetcode/medium/phase2/Number144.java
@@ -0,0 +1,94 @@
+package leetcode.medium.phase2;
+
+import method.Iteration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author qcy
+ * @create 2021/01/06 19:56:18
+ * 144. 二叉树的前序遍历
+ *
+ * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+ * 
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,2,3]
+ * 示例 2：
+ *
+ * 输入：root = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：root = [1]
+ * 输出：[1]
+ * 示例 4：
+ *
+ *
+ * 输入：root = [1,2]
+ * 输出：[1,2]
+ * 示例 5：
+ *
+ *
+ * 输入：root = [1,null,2]
+ * 输出：[1,2]
+ *  
+ *
+ * 提示：
+ *
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
+ *  
+ *
+ * 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ */
+public class Number144 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    @Iteration
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return list;
+    }
+
+}
