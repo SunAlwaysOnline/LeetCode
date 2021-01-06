68078da8f2fd66dc1490de1ead371941cf170937
SunAlwaysOnline <767638734@qq.com>
2021-01-06 17:54:00 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()第693题

---
+++ src/leetcode/easy/phase7/Number693.java
@@ -0,0 +1,66 @@
+package leetcode.easy.phase7;
+
+import method.BitOperation;
+import method.Classic;
+
+/**
+ * @author qcy
+ * @create 2021/01/06 17:43:11
+ * 693. 交替位二进制数
+ *
+ * 给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：换句话说，就是二进制表示中相邻两位的数字永不相同。
+ * 
+ *
+ * 示例 1：
+ *
+ * 输入：n = 5
+ * 输出：true
+ * 解释：5 的二进制表示是：101
+ * 示例 2：
+ *
+ * 输入：n = 7
+ * 输出：false
+ * 解释：7 的二进制表示是：111.
+ * 示例 3：
+ *
+ * 输入：n = 11
+ * 输出：false
+ * 解释：11 的二进制表示是：1011.
+ * 示例 4：
+ *
+ * 输入：n = 10
+ * 输出：true
+ * 解释：10 的二进制表示是：1010.
+ * 示例 5：
+ *
+ * 输入：n = 3
+ * 输出：false
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= n <= 231 - 1
+ *
+ */
+@Classic
+public class Number693 {
+
+    @BitOperation
+    public boolean hasAlternatingBits(int n) {
+        int pre = -1;
+        while (n != 0) {
+            int cur = n & 1;
+            if (cur == pre) {
+                return false;
+            }
+            pre = cur;
+            n = n >> 1;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Number693().hasAlternatingBits(3));
+    }
+
+}
