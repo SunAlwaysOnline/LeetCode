07eb821b25d15a50298752e4fb1dbcb861bba5e6
SunAlwaysOnline <767638734@qq.com>
2021-01-07 09:48:45 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()第547题

--- src/method/UF.java
+++ src/method/UF.java
@@ -1,9 +1,14 @@
 package method;
 
+import java.lang.annotation.*;
+
 /**
  * @author qcy
  * @create 2020/09/28 14:52:26
  * 并查集
  */
-public interface UF {
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface UF {
 }
