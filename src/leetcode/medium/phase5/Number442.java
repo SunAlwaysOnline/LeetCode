442192c21512e52f4a6079162e5a46efd358e16f
SunAlwaysOnline <767638734@qq.com>
2021-01-11 15:03:51 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()第442题

---
+++ src/leetcode/medium/phase5/Number442.java
@@ -0,0 +1,66 @@
+package leetcode.medium.phase5;
+
+import method.Classic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author qcy
+ * @create 2021/01/11 14:54:44
+ * 442. 数组中重复的数据
+ *
+ * 给定一个整数数组 a，其中1 ≤ a[i] ≤ n （n为数组长度）, 其中有些元素出现两次而其他元素出现一次。
+ *
+ * 找到所有出现两次的元素。
+ *
+ * 你可以不用到任何额外空间并在O(n)时间复杂度内解决这个问题吗？
+ *
+ * 示例：
+ *
+ * 输入:
+ * [4,3,2,7,8,2,3,1]
+ *
+ * 输出:
+ * [2,3]
+ *
+ */
+@Classic
+public class Number442 {
+
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        int len = nums.length;
+        if (len == 0) {
+            return res;
+        }
+        for (int i = 0; i < len; i++) {
+            while (nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            if (nums[i] - 1 != i) {
+                res.add(nums[i]);
+            }
+        }
+        return res;
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        if (index1 == index2) {
+            return;
+        }
+        nums[index1] = nums[index1] ^ nums[index2];
+        nums[index2] = nums[index1] ^ nums[index2];
+        nums[index1] = nums[index1] ^ nums[index2];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        for (Integer duplicate : new Number442().findDuplicates(nums)) {
+            System.out.println(duplicate);
+        }
+    }
+
+}
