2c4094638fc92f903088c8f507b38918d8daf226
SunAlwaysOnline <767638734@qq.com>
2021-01-12 16:55:55 +0800

    add:提交leetcode()|jzoffer(√)|interview()|lcp()|sql()第59-1题

---
+++ src/jzoffer/easy/phase1/Number59dot1.java
@@ -0,0 +1,24 @@
+package jzoffer.easy.phase1;
+
+import method.WaitForComplete;
+
+/**
+ * @author qcy
+ * @create 2021/01/12 16:33:06
+ */
+@WaitForComplete
+public class Number59dot1 {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] res = new int[nums.length - k + 1];
+        int index = 0;
+
+
+        for (int i = 0; i < nums.length - k + 1; i++) {
+
+        }
+
+        return res;
+    }
+
+}
