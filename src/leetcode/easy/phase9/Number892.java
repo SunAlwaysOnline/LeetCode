b709cf8f234365da4609f9b43c6483d690fc741b
SunAlwaysOnline <767638734@qq.com>
2021-01-12 14:12:50 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()第892题

---
+++ src/leetcode/easy/phase9/Number892.java
@@ -0,0 +1,16 @@
+package leetcode.easy.phase9;
+
+import method.WaitForComplete;
+
+/**
+ * @author qcy
+ * @create 2021/01/12 14:07:45
+ */
+@WaitForComplete
+public class Number892 {
+
+    public int surfaceArea(int[][] grid) {
+        return -1;
+    }
+
+}
