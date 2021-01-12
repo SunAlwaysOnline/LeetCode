7ecd1bc24932ccbecff61def49e1923bb9553439
SunAlwaysOnline <767638734@qq.com>
2021-01-12 10:06:48 +0800

    add:提交leetcode()|jzoffer()|interview(√)|lcp()|sql()第17.01题

---
+++ src/interview/easy/phase17/Number17dot01.java
@@ -0,0 +1,18 @@
+package interview.easy.phase17;
+
+import method.BitOperation;
+import method.WaitForComplete;
+
+/**
+ * @author qcy
+ * @create 2021/01/12 10:04:48
+ */
+@BitOperation
+@WaitForComplete
+public class Number17dot01 {
+
+    public int add(int a, int b) {
+        return -1;
+    }
+
+}
