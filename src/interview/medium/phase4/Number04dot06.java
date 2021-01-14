ea0aa51263b344a22c6b5223e98ddc697013ac06
SunAlwaysOnline <767638734@qq.com>
2021-01-14 14:05:24 +0800

    add:提交leetcode()|jzoffer()|interview(√)|lcp()|sql()第04.06题

---
+++ src/interview/medium/phase4/Number04dot06.java
@@ -0,0 +1,65 @@
+package interview.medium.phase4;
+
+import datastructure.BinaryTree;
+
+/**
+ * @author qcy
+ * @create 2021/01/14 11:28:04
+ * 面试题 04.06. 后继者
+ *
+ * 设计一个算法，找出二叉搜索树中指定节点的“下一个”节点（也即中序后继）。
+ *
+ * 如果指定节点没有对应的“下一个”节点，则返回null。
+ *
+ * 示例 1:
+ *
+ * 输入: root = [2,1,3], p = 1
+ *
+ *   2
+ *  / \
+ * 1   3
+ *
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: root = [5,3,6,2,4,null,null,1], p = 6
+ *
+ *       5
+ *      / \
+ *     3   6
+ *    / \
+ *   2   4
+ *  /
+ * 1
+ *
+ * 输出: null
+ *
+ */
+@BinaryTree
+public class Number04dot06 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null || p == null) {
+            return null;
+        }
+        if (p.val >= root.val) {
+            return inorderSuccessor(root.right, p);
+        }
+        TreeNode left = inorderSuccessor(root.left, p);
+        if (left == null) {
+            return root;
+        }
+        return left;
+    }
+
+}
