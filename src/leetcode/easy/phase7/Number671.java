b9a52674efd3283dd5d7343eb44667f0bb96a8b1
SunAlwaysOnline <767638734@qq.com>
2021-01-14 18:11:15 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()第671题

---
+++ src/leetcode/easy/phase7/Number671.java
@@ -0,0 +1,92 @@
+package leetcode.easy.phase7;
+
+import datastructure.BinaryTree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author qcy
+ * @create 2021/01/14 17:43:25
+ * 671. 二叉树中第二小的节点
+ *
+ * 给定一个非空特殊的二叉树，每个节点都是正数，并且每个节点的子节点数量只能为 2 或 0。如果一个节点有两个子节点的话，那么该节点的值等于两个子节点中较小的一个。
+ *
+ * 更正式地说，root.val = min(root.left.val, root.right.val) 总成立。
+ *
+ * 给出这样的一个二叉树，你需要输出所有节点中的第二小的值。如果第二小的值不存在的话，输出 -1 。
+ * 
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [2,2,5,null,null,5,7]
+ * 输出：5
+ * 解释：最小的值是 2 ，第二小的值是 5 。
+ * 示例 2：
+ *
+ *
+ * 输入：root = [2,2,2]
+ * 输出：-1
+ * 解释：最小的值是 2, 但是不存在第二小的值。
+ *  
+ *
+ * 提示：
+ *
+ * 树中节点数目在范围 [1, 25] 内
+ * 1 <= Node.val <= 231 - 1
+ * 对于树中每个节点 root.val == min(root.left.val, root.right.val)
+ *
+ */
+@BinaryTree
+public class Number671 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int findSecondMinimumValue(TreeNode root) {
+        int min = root.val;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                int val = node.val;
+
+                if (min == root.val) {
+                    min = val;
+                } else if (val != root.val) {
+                    min = Math.min(min, val);
+                }
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return min == root.val ? -1 : min;
+    }
+
+}
