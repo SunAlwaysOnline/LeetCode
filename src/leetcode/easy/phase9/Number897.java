6824285a3d09355759217fa153a1e016ad741461
SunAlwaysOnline <767638734@qq.com>
2021-01-15 14:12:56 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()第897题

---
+++ src/leetcode/easy/phase9/Number897.java
@@ -0,0 +1,104 @@
+package leetcode.easy.phase9;
+
+import datastructure.BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author qcy
+ * @create 2021/01/15 13:56:13
+ * 897. 递增顺序查找树
+ *
+ * 给你一个树，请你 按中序遍历 重新排列树，使树中最左边的结点现在是树的根，并且每个结点没有左子结点，只有一个右子结点。
+ *
+ *  
+ *
+ * 示例 ：
+ *
+ * 输入：[5,3,6,2,4,null,8,1,null,null,null,7,9]
+ *
+ *        5
+ *       / \
+ *     3    6
+ *    / \    \
+ *   2   4    8
+ *  /        / \
+ * 1        7   9
+ *
+ * 输出：[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+ *
+ *  1
+ *   \
+ *    2
+ *     \
+ *      3
+ *       \
+ *        4
+ *         \
+ *          5
+ *           \
+ *            6
+ *             \
+ *              7
+ *               \
+ *                8
+ *                 \
+ *                  9
+ *  
+ *
+ * 提示：
+ *
+ * 给定树中的结点数介于 1 和 100 之间。
+ * 每个结点都有一个从 0 到 1000 范围内的唯一整数值。
+ *
+ */
+@BinaryTree
+public class Number897 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    List<Integer> list = new ArrayList<>();
+
+    public TreeNode increasingBST(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        dfs(root);
+
+        TreeNode newRoot = new TreeNode(list.get(0));
+        TreeNode cur = newRoot;
+        for (int i = 1; i < list.size(); i++) {
+            cur.right = new TreeNode(list.get(i));
+            cur = cur.right;
+        }
+        return newRoot;
+    }
+
+    private void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        dfs(node.left);
+        list.add(node.val);
+        dfs(node.right);
+    }
+
+}
