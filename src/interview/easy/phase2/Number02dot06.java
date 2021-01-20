76dddb22b279f72fbd8b955b0aa4653b82780c92
SunAlwaysOnline <767638734@qq.com>
2021-01-20 19:02:43 +0800

    add:提交leetcode()|jzoffer()|interview(√)|lcp()|sql()|notice()第02-06题

--- src/interview/easy/phase2/Number02dot06.java
+++ src/interview/easy/phase2/Number02dot06.java
@@ -1,9 +1,35 @@
 package interview.easy.phase2;
 
+import datastructure.LinkedList;
+import method.Classic;
+
 /**
  * @author qcy
  * @create 2021/01/20 18:02:00
+ * 面试题 02.06. 回文链表
+ *
+ * 编写一个函数，检查输入的链表是否是回文的。
+ * 
+ *
+ * 示例 1：
+ *
+ * 输入： 1->2
+ * 输出： false
+ * 示例 2：
+ *
+ * 输入： 1->2->2->1
+ * 输出： true
+ *  
+ *
+ * 进阶：
+ * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindrome-linked-list-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
+@Classic
+@LinkedList
 public class Number02dot06 {
 
     public class ListNode {
@@ -24,7 +50,33 @@ public class Number02dot06 {
 
         //找到中间节点
         ListNode middle = findMiddleNode(head);
-        return false;
+        //反转链表
+        ListNode middleNext = reverse(middle.next);
+
+        ListNode n1 = head;
+        ListNode n2 = middleNext;
+
+        while (n2 != null) {
+            if (n1.val != n2.val) {
+                return false;
+            }
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+
+        while (cur != null) {
+            ListNode curNext = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = curNext;
+        }
+        return pre;
     }
 
     private ListNode findMiddleNode(ListNode head) {
