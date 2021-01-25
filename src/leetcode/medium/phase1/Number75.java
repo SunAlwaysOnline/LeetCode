2456ade021c5d9846e82fa6c37294d6d5e66d527
SunAlwaysOnline <767638734@qq.com>
2021-01-25 13:43:29 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()|notice()第75题

---
+++ src/leetcode/medium/phase1/Number75.java
@@ -0,0 +1,92 @@
+package leetcode.medium.phase1;
+
+import datastructure.Array;
+import method.Classic;
+
+/**
+ * @author qcy
+ * @create 2021/01/25 11:36:09
+ * 75. 颜色分类
+ *
+ * 给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，
+ * 并按照红色、白色、蓝色顺序排列。
+ *
+ * 此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,0,2,1,1,0]
+ * 输出：[0,0,1,1,2,2]
+ * 示例 2：
+ *
+ * 输入：nums = [2,0,1]
+ * 输出：[0,1,2]
+ * 示例 3：
+ *
+ * 输入：nums = [0]
+ * 输出：[0]
+ * 示例 4：
+ *
+ * 输入：nums = [1]
+ * 输出：[1]
+ *  
+ *
+ * 提示：
+ *
+ * n == nums.length
+ * 1 <= n <= 300
+ * nums[i] 为 0、1 或 2
+ *  
+ *
+ * 进阶：
+ *
+ * 你可以不使用代码库中的排序函数来解决这道题吗？
+ * 你能想出一个仅使用常数空间的一趟扫描算法吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/sort-colors
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Classic
+@Array
+public class Number75 {
+
+    public void sortColors(int[] nums) {
+        int p0 = 0;
+        int p1 = 0;
+        int length = nums.length;
+
+        for (int i = 0; i < length; i++) {
+            if (nums[i] == 0) {
+                swap(nums, p0, i);
+                if (p0 < p1) {
+                    swap(nums, p1, i);
+                }
+                p0++;
+                p1++;
+                continue;
+            }
+            if (nums[i] == 1) {
+                swap(nums, p1, i);
+                p1++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 1, 0, 2, 1, 0};
+        new Number75().sortColors(nums);
+        for (int num : nums) {
+            System.out.println(num + ",");
+        }
+    }
+
+}
