ca9992821f90b8a77e4a725d6bfe660faab4eb32
SunAlwaysOnline <767638734@qq.com>
2021-01-27 14:24:47 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()|notice()第653题

---
+++ src/leetcode/easy/phase7/Number653.java
@@ -0,0 +1,86 @@
+package leetcode.easy.phase7;
+
+import datastructure.BinaryTree;
+import method.Classic;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author qcy
+ * @create 2021/01/27 14:12:52
+ * 653. 两数之和 IV - 输入 BST
+ *
+ * 给定一个二叉搜索树和一个目标结果，如果 BST 中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+ *
+ * 案例 1:
+ *
+ * 输入:
+ *     5
+ *    / \
+ *   3   6
+ *  / \   \
+ * 2   4   7
+ *
+ * Target = 9
+ *
+ * 输出: True
+ *  
+ *
+ * 案例 2:
+ *
+ * 输入:
+ *     5
+ *    / \
+ *   3   6
+ *  / \   \
+ * 2   4   7
+ *
+ * Target = 28
+ *
+ * 输出: False
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Classic
+@BinaryTree
+public class Number653 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean findTarget(TreeNode root, int k) {
+        Set<Integer> set = new HashSet<>();
+        return find(root, set, k);
+    }
+
+    private boolean find(TreeNode node, Set<Integer> set, int k) {
+        if (node == null) {
+            return false;
+        }
+        if (set.contains(k - node.val)) {
+            return true;
+        }
+        set.add(node.val);
+        return find(node.left, set, k) || find(node.right, set, k);
+    }
+
+}
