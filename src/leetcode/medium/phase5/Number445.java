4a70d774d4f8432f810883830cbe5cd74dbe33d3
SunAlwaysOnline <767638734@qq.com>
2021-01-28 11:11:02 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()|notice()第445题

---
+++ src/leetcode/medium/phase5/Number445.java
@@ -0,0 +1,76 @@
+package leetcode.medium.phase5;
+
+import datastructure.LinkedList;
+import method.Classic;
+
+import java.util.Stack;
+
+/**
+ * @author qcy
+ * @create 2021/01/28 10:55:54
+ * 445. 两数相加 II
+ *
+ * 给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。
+ * 它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。
+ *
+ * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+ *
+ *  
+ *
+ * 进阶：
+ *
+ * 如果输入链表不能修改该如何处理？换句话说，你不能对列表中的节点进行翻转。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 输入：(7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 8 -> 0 -> 7
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Classic
+@LinkedList
+public class Number445 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        ListNode head = null;
+        boolean carry = false;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry) {
+            int sum = carry ? 1 : 0;
+            sum += stack1.isEmpty() ? 0 : stack1.pop();
+            sum += stack2.isEmpty() ? 0 : stack2.pop();
+            carry = sum > 9;
+
+            ListNode node = new ListNode(sum % 10);
+            node.next = head;
+            head = node;
+        }
+        return head;
+    }
+
+}
