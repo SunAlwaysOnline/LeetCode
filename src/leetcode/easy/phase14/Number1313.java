c394f0adb6a9c2cdc131dd4af9efb464638e6d74
SunAlwaysOnline <767638734@qq.com>
2021-02-02 16:20:58 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()|notice()第1313题

---
+++ src/leetcode/easy/phase14/Number1313.java
@@ -0,0 +1,60 @@
+package leetcode.easy.phase14;
+
+import datastructure.Array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author qcy
+ * @create 2021/02/02 16:18:14
+ * 1313. 解压缩编码列表
+ *
+ * 给你一个以行程长度编码压缩的整数列表 nums 。
+ *
+ * 考虑每对相邻的两个元素 [freq, val] = [nums[2*i], nums[2*i+1]] （其中 i >= 0 ），
+ * 每一对都表示解压后子列表中有 freq 个值为 val 的元素，你需要从左到右连接所有子列表以生成解压后的列表。
+ *
+ * 请你返回解压后的列表。
+ *  
+ *
+ * 示例：
+ *
+ * 输入：nums = [1,2,3,4]
+ * 输出：[2,4,4,4]
+ * 解释：第一对 [1,2] 代表着 2 的出现频次为 1，所以生成数组 [2]。
+ * 第二对 [3,4] 代表着 4 的出现频次为 3，所以生成数组 [4,4,4]。
+ * 最后将它们串联到一起 [2] + [4,4,4] = [2,4,4,4]。
+ * 示例 2：
+ *
+ * 输入：nums = [1,1,2,3]
+ * 输出：[1,3,3]
+ *  
+ *
+ * 提示：
+ *
+ * 2 <= nums.length <= 100
+ * nums.length % 2 == 0
+ * 1 <= nums[i] <= 100
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/decompress-run-length-encoded-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Array
+public class Number1313 {
+
+    public int[] decompressRLElist(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length - 1; i += 2) {
+            int freq = nums[i];
+            int val = nums[i + 1];
+            while (freq != 0) {
+                list.add(val);
+                freq--;
+            }
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+}
