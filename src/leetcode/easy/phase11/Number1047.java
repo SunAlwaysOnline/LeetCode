ce850c14675eeb62c5acb0cf1c04db60c7821841
SunAlwaysOnline <767638734@qq.com>
2021-02-12 17:08:06 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|notice()|sql()第1047题

---
+++ src/leetcode/easy/phase11/Number1047.java
@@ -0,0 +1,81 @@
+package leetcode.easy.phase11;
+
+import method.StringOperation;
+
+import java.util.Stack;
+
+/**
+ * @author qcy
+ * @create 2021/02/12 16:43:06
+ * 1047. 删除字符串中的所有相邻重复项
+ *
+ * 给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。
+ *
+ * 在 S 上反复执行重复项删除操作，直到无法继续删除。
+ *
+ * 在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。
+ *
+ *
+ * 示例：
+ *
+ * 输入："abbaca"
+ * 输出："ca"
+ * 解释：
+ * 例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。
+ * 之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= S.length <= 20000
+ * S 仅由小写英文字母组成。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@StringOperation
+@datastructure.Stack
+public class Number1047 {
+
+    @datastructure.Stack
+    public String removeDuplicates1(String S) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : S.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(ch);
+                continue;
+            }
+            if (stack.peek() == ch) {
+                stack.pop();
+            } else {
+                stack.push(ch);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+
+    public String removeDuplicates(String S) {
+        StringBuilder sb = new StringBuilder();
+        int length = sb.length();
+        for (char ch : S.toCharArray()) {
+            if (length != 0 && ch == sb.charAt(length - 1)) {
+                sb.deleteCharAt(length - 1);
+                length--;
+            } else {
+                sb.append(ch);
+                length++;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Number1047().removeDuplicates("abbaca"));
+    }
+
+}
