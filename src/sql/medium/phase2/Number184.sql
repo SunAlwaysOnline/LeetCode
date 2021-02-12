bdfe57a0064fbd12f582a889c50f8950ae775916
SunAlwaysOnline <767638734@qq.com>
2021-02-12 15:30:44 +0800

    add:提交leetcode()|jzoffer()|interview()|lcp()|notice()|sql(√)第184题

---
+++ src/sql/medium/phase2/Number184.sql
@@ -0,0 +1,49 @@
+-- 184. 部门工资最高的员工
+
+
+-- Employee 表包含所有员工信息，每个员工有其对应的 Id, salary 和 department Id。
+--
+-- +----+-------+--------+--------------+
+-- | Id | Name  | Salary | DepartmentId |
+-- +----+-------+--------+--------------+
+-- | 1  | Joe   | 70000  | 1            |
+-- | 2  | Jim   | 90000  | 1            |
+-- | 3  | Henry | 80000  | 2            |
+-- | 4  | Sam   | 60000  | 2            |
+-- | 5  | Max   | 90000  | 1            |
+-- +----+-------+--------+--------------+
+-- Department 表包含公司所有部门的信息。
+--
+-- +----+----------+
+-- | Id | Name     |
+-- +----+----------+
+-- | 1  | IT       |
+-- | 2  | Sales    |
+-- +----+----------+
+-- 编写一个 SQL 查询，找出每个部门工资最高的员工。对于上述表，您的 SQL 查询应返回以下行（行的顺序无关紧要）。
+--
+-- +------------+----------+--------+
+-- | Department | Employee | Salary |
+-- +------------+----------+--------+
+-- | IT         | Max      | 90000  |
+-- | IT         | Jim      | 90000  |
+-- | Sales      | Henry    | 80000  |
+-- +------------+----------+--------+
+-- 解释：
+--
+-- Max 和 Jim 在 IT 部门的工资都是最高的，Henry 在销售部的工资最高。
+--
+-- 来源：力扣（LeetCode）
+-- 链接：https://leetcode-cn.com/problems/department-highest-salary
+-- 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+
+SELECT
+	d.NAME AS Department,
+	e.NAME AS Employee,
+	e.Salary AS Salary
+FROM
+	Employee AS e
+	JOIN Department AS d ON e.DepartmentId = d.Id
+	AND ( e.Salary, e.DepartmentId ) IN ( SELECT max( Salary ), DepartmentId FROM Employee GROUP BY DepartmentId );
