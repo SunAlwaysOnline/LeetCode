7753f075aacab444630b9c4e2d771189da9487a1
SunAlwaysOnline <767638734@qq.com>
2021-02-17 21:35:12 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|notice()|sql()第34题

---
+++ src/leetcode/medium/phase1/Number34.java
@@ -0,0 +1,106 @@
+package leetcode.medium.phase1;
+
+import complexity.SpaceComplexity;
+import complexity.TimeComplexity;
+import datastructure.Array;
+import method.Classic;
+
+/**
+ * @author qcy
+ * @create 2021/02/17 21:04:59
+ * 34. 在排序数组中查找元素的第一个和最后一个位置
+ *
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ *
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ *
+ * 进阶：
+ *
+ * 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [5,7,7,8,8,10], target = 8
+ * 输出：[3,4]
+ * 示例 2：
+ *
+ * 输入：nums = [5,7,7,8,8,10], target = 6
+ * 输出：[-1,-1]
+ * 示例 3：
+ *
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
+ *  
+ *
+ * 提示：
+ *
+ * 0 <= nums.length <= 105
+ * -109 <= nums[i] <= 109
+ * nums 是一个非递减数组
+ * -109 <= target <= 109
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Classic
+@Array
+public class Number34 {
+
+    @SpaceComplexity("O(1)")
+    @TimeComplexity("O(logn)")
+    public int[] searchRange(int[] nums, int target) {
+        if (nums.length == 0) {
+            return new int[]{-1, -1};
+        }
+
+        int firstPosition = findFirstPosition(nums, target);
+        if (firstPosition == -1) {
+            return new int[]{-1, -1};
+        }
+        int lastPosition = findLastPosition(nums, target);
+        return new int[]{firstPosition, lastPosition};
+    }
+
+    private int findFirstPosition(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] == target || nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        if (left != nums.length && nums[left] == target) {
+            return left;
+        }
+        return -1;
+    }
+
+    private int findLastPosition(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] == target || nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+
+        return right;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        new Number34().searchRange(nums, 8);
+    }
+
+}
