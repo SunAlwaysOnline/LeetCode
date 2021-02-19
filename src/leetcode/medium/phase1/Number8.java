7fb20673042daf23170582850bda33968fdcc334
SunAlwaysOnline <767638734@qq.com>
2021-02-19 17:14:35 +0800

    add:提交leetcode()|jzoffer(√)|interview()|lcp()|notice()|sql()第04题

---
+++ src/leetcode/medium/phase1/Number8.java
@@ -0,0 +1,20 @@
+package leetcode.medium.phase1;
+
+import method.Classic;
+import method.StringOperation;
+import method.WaitForComplete;
+
+/**
+ * @author qcy
+ * @create 2021/02/19 17:00:27
+ */
+@Classic
+@StringOperation
+@WaitForComplete
+public class Number8 {
+
+    public int myAtoi(String s) {
+        return -1;
+    }
+
+}
