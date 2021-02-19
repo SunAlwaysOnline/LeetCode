49148d65f4b2a29392b2da75e0ddd33e85ac5842
SunAlwaysOnline <767638734@qq.com>
2021-02-19 21:31:02 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|notice()|sql()第572题

--- src/leetcode/easy/phase6/Number572.java
+++ src/leetcode/easy/phase6/Number572.java
@@ -1,34 +1,102 @@
-//package leetcode.easy.phase6;
-//
-///**
-// * @author qcy
-// * @create 2020/12/04 17:28:45
-// */
-//public class Number572 {
-//
-//    public class TreeNode {
-//        int val;
-//        TreeNode left;
-//        TreeNode right;
-//
-//        TreeNode() {
-//        }
-//
-//        TreeNode(int val) {
-//            this.val = val;
-//        }
-//
-//        TreeNode(int val, TreeNode left, TreeNode right) {
-//            this.val = val;
-//            this.left = left;
-//            this.right = right;
-//        }
-//    }
-//
-//    public boolean isSubtree(TreeNode s, TreeNode t) {
-//
-//    }
-//
-//
-//
-//}
+package leetcode.easy.phase6;
+
+import complexity.SpaceComplexity;
+import complexity.TimeComplexity;
+import datastructure.BinaryTree;
+import method.Classic;
+
+/**
+ * @author qcy
+ * @create 2020/12/04 17:28:45
+ * 572. 另一个树的子树
+ *
+ * 给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。
+ * s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树。
+ *
+ * 示例 1:
+ * 给定的树 s:
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ * 给定的树 t：
+ *
+ *    4
+ *   / \
+ *  1   2
+ * 返回 true，因为 t 与 s 的一个子树拥有相同的结构和节点值。
+ *
+ * 示例 2:
+ * 给定的树 s：
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ *     /
+ *    0
+ * 给定的树 t：
+ *
+ *    4
+ *   / \
+ *  1   2
+ * 返回 false。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/subtree-of-another-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Classic
+@BinaryTree
+public class Number572 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    @SpaceComplexity("O(max{m,n})")//m,n分别为s,t的高度
+    @TimeComplexity("O(s*t)")//s,t为对应树的节点数
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        return dfs(s, t);
+    }
+
+    private boolean dfs(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        return isSametree(s, t) || dfs(s.left, t) || dfs(s.right, t);
+    }
+
+    private boolean isSametree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.val != t.val) {
+            return false;
+        }
+        return isSametree(s.left, t.left) && isSametree(s.right, t.right);
+    }
+
+
+}
