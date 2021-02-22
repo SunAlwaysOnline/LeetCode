25cb7e9fb3332263c91cd9b4bd61ba7e8e83401b
SunAlwaysOnline <767638734@qq.com>
2021-02-22 09:27:34 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()|notice()第766题

---
+++ src/leetcode/easy/phase8/Number766.java
@@ -0,0 +1,68 @@
+package leetcode.easy.phase8;
+
+import datastructure.Array;
+
+/**
+ * @author qcy
+ * @create 2021/02/22 09:19:21
+ * 766. 托普利茨矩阵
+ *
+ * 给你一个 m x n 的矩阵 matrix 。如果这个矩阵是托普利茨矩阵，返回 true ；否则，返回 false 。
+ *
+ * 如果矩阵上每一条由左上到右下的对角线上的元素都相同，那么这个矩阵是 托普利茨矩阵 。
+ * 
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+ * 输出：true
+ * 解释：
+ * 在上述矩阵中, 其对角线为:
+ * "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]"。
+ * 各条对角线上的所有元素均相同, 因此答案是 True 。
+ * 示例 2：
+ *
+ *
+ * 输入：matrix = [[1,2],[2,2]]
+ * 输出：false
+ * 解释：
+ * 对角线 "[1, 2]" 上的元素不同。
+ *  
+ *
+ * 提示：
+ *
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 20
+ * 0 <= matrix[i][j] <= 99
+ *  
+ *
+ * 进阶：
+ *
+ * 如果矩阵存储在磁盘上，并且内存有限，以至于一次最多只能将矩阵的一行加载到内存中，该怎么办？
+ * 如果矩阵太大，以至于一次只能将不完整的一行加载到内存中，该怎么办？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/toeplitz-matrix
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Array
+public class Number766 {
+
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+}
