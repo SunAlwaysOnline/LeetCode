6c2a15fd57bc80df63abfef9632e784f58bde411
SunAlwaysOnline <767638734@qq.com>
2021-02-26 10:59:59 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()|notice()第337题

---
+++ src/leetcode/medium/phase4/Number322.java
@@ -0,0 +1,18 @@
+package leetcode.medium.phase4;
+
+import method.Classic;
+import method.WaitForComplete;
+
+/**
+ * @author qcy
+ * @create 2021/02/26 10:12:23
+ */
+@Classic
+@WaitForComplete
+public class Number322 {
+
+    public int coinChange(int[] coins, int amount) {
+        return -1;
+    }
+
+}
---
+++ src/leetcode/medium/phase4/Number337.java
@@ -0,0 +1,83 @@
+package leetcode.medium.phase4;
+
+import datastructure.BinaryTree;
+import method.Classic;
+import method.DP;
+
+/**
+ * @author qcy
+ * @create 2021/02/26 10:53:15
+ * 337. 打家劫舍 III
+ *
+ * 在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。
+ * 这个地区只有一个入口，我们称之为“根”。 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。
+ * 一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。
+ * 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+ *
+ * 计算在不触动警报的情况下，小偷一晚能够盗取的最高金额。
+ *
+ * 示例 1:
+ *
+ * 输入: [3,2,3,null,3,null,1]
+ *
+ *      3
+ *     / \
+ *    2   3
+ *     \   \
+ *      3   1
+ *
+ * 输出: 7
+ * 解释: 小偷一晚能够盗取的最高金额 = 3 + 3 + 1 = 7.
+ * 示例 2:
+ *
+ * 输入: [3,4,5,1,3,null,1]
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \   \
+ *  1   3   1
+ *
+ * 输出: 9
+ * 解释: 小偷一晚能够盗取的最高金额 = 4 + 5 = 9.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/house-robber-iii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Classic
+@BinaryTree
+public class Number337 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    @DP
+    public int rob(TreeNode root) {
+        int[] res = dfs(root);
+        return Math.max(res[0], res[1]);
+    }
+
+    private int[] dfs(TreeNode node) {
+        int[] res = new int[2];
+        if (node == null) {
+            return res;
+        }
+
+        int[] left = dfs(node.left);
+        int[] right = dfs(node.right);
+
+        //0 不偷, 1 偷
+        res[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        res[1] = node.val + left[0] + right[0];
+        return res;
+    }
+
+}
