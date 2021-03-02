2cd07756257f5e70310dd9aa24fa3e52851a4852
SunAlwaysOnline <767638734@qq.com>
2021-03-02 16:31:58 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()|notice()第124题

---
+++ src/leetcode/hard/phase2/Number124.java
@@ -0,0 +1,92 @@
+package leetcode.hard.phase2;
+
+import datastructure.BinaryTree;
+
+/**
+ * @author qcy
+ * @create 2021/03/02 16:11:09
+ * 124. 二叉树中的最大路径和
+ *
+ * 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。
+ * 同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含一个 节点，且不一定经过根节点。
+ *
+ * 路径和 是路径中各节点值的总和。
+ *
+ * 给你一个二叉树的根节点 root ，返回其 最大路径和 。
+ * 
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,2,3]
+ * 输出：6
+ * 解释：最优路径是 2 -> 1 -> 3 ，路径和为 2 + 1 + 3 = 6
+ * 示例 2：
+ *
+ *
+ * 输入：root = [-10,9,20,null,null,15,7]
+ * 输出：42
+ * 解释：最优路径是 15 -> 20 -> 7 ，路径和为 15 + 20 + 7 = 42
+ *  
+ *
+ * 提示：
+ *
+ * 树中节点数目范围是 [1, 3 * 104]
+ * -1000 <= Node.val <= 1000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-maximum-path-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@BinaryTree
+public class Number124 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    int max = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        dfs(root);
+        return max;
+    }
+
+    //以该节点为开始节点的一侧最大和
+    private int dfs(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+        max = getMax(max, node.val, left + node.val, right + node.val, left + right + node.val);
+        return getMax(node.val, left + node.val, right + node.val);
+    }
+
+    private int getMax(int... nums) {
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);
+        }
+        return max;
+    }
+
+}
