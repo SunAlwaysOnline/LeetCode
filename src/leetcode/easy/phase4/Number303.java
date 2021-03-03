2ab8ced3fbe2cd2a4d63c574aa967afa4c5db0e8
SunAlwaysOnline <767638734@qq.com>
2021-03-03 16:59:21 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()|notice()第303题

---
+++ src/leetcode/easy/phase4/Number303.java
@@ -0,0 +1,80 @@
+package leetcode.easy.phase4;
+
+import datastructure.Array;
+import method.Classic;
+import method.PrefixSum;
+
+/**
+ * @author qcy
+ * @create 2021/03/03 16:53:11
+ * 303. 区域和检索 - 数组不可变
+ *
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点。
+ *
+ * 实现 NumArray 类：
+ *
+ * NumArray(int[] nums) 使用数组 nums 初始化对象
+ * int sumRange(int i, int j) 返回数组 nums 从索引 i 到 j（i ≤ j）范围内元素的总和，
+ * 包含 i、j 两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
+ *  
+ *
+ * 示例：
+ *
+ * 输入：
+ * ["NumArray", "sumRange", "sumRange", "sumRange"]
+ * [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+ * 输出：
+ * [null, 1, -1, -3]
+ *
+ * 解释：
+ * NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+ * numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+ * numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1))
+ * numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+ *  
+ *
+ * 提示：
+ *
+ * 0 <= nums.length <= 104
+ * -105 <= nums[i] <= 105
+ * 0 <= i <= j < nums.length
+ * 最多调用 104 次 sumRange 方法
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/range-sum-query-immutable
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Classic
+@Array
+@PrefixSum
+public class Number303 {
+
+    class NumArray {
+        int[] nums;
+        int length;
+        int[] prefixSum;
+
+        public NumArray(int[] nums) {
+            length = nums.length;
+            this.nums = nums;
+            if (length == 0) {
+                return;
+            }
+
+            prefixSum = new int[length];
+            prefixSum[0] = nums[0];
+
+            for (int i = 1; i < length; i++) {
+                prefixSum[i] = prefixSum[i - 1] + nums[i];
+            }
+        }
+
+        public int sumRange(int i, int j) {
+            if (length == 0) {
+                return 0;
+            }
+            return prefixSum[j] - prefixSum[i] + nums[i];
+        }
+    }
+
+}
