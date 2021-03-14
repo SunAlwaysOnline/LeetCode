8dd9351233299aa7ab025fd606bd6ce7a4f8a0b8
SunAlwaysOnline <767638734@qq.com>
2021-03-14 18:56:38 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|notice()|sql()第1382题

---
+++ src/leetcode/medium/phase14/Number1382.java
@@ -0,0 +1,79 @@
+package leetcode.medium.phase14;
+
+import datastructure.BinaryTree;
+import method.Classic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author qcy
+ * @create 2021/03/14 18:47:10
+ * 1382. 将二叉搜索树变平衡
+ *
+ * 给你一棵二叉搜索树，请你返回一棵 平衡后 的二叉搜索树，新生成的树应该与原来的树有着相同的节点值。
+ *
+ * 如果一棵二叉搜索树中，每个节点的两棵子树高度差不超过 1 ，我们就称这棵二叉搜索树是 平衡的 。
+ *
+ * 如果有多种构造方法，请你返回任意一种。
+ * 
+ *
+ * 示例：
+ *
+ *
+ * 输入：root = [1,null,2,null,3,null,4,null,null]
+ * 输出：[2,1,3,null,null,null,4]
+ * 解释：这不是唯一的正确答案，[3,1,4,null,2,null,null] 也是一个可行的构造方案。
+ *  
+ *
+ * 提示：
+ *
+ * 树节点的数目在 1 到 10^4 之间。
+ * 树节点的值互不相同，且在 1 到 10^5 之间。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/balance-a-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Classic
+@BinaryTree
+public class Number1382 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    List<Integer> list = new ArrayList<>();
+
+    public TreeNode balanceBST(TreeNode root) {
+        inorder(root);
+        return build(0, list.size() - 1);
+    }
+
+    private void inorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left);
+        list.add(node.val);
+        inorder(node.right);
+    }
+
+    private TreeNode build(int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int middle = (left + right) / 2;
+        TreeNode root = new TreeNode(list.get(middle));
+        root.left = build(left, middle - 1);
+        root.right = build(middle + 1, right);
+        return root;
+    }
+
+}
