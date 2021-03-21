3f77ceca04c8dd9cab5a79b8763a88f68f565393
SunAlwaysOnline <767638734@qq.com>
2021-03-21 22:32:01 +0800

    add:提交leetcode()|jzoffer()|interview(√)|lcp()|notice()|sql()第02.01题

---
+++ src/interview/easy/phase2/Number02dot01.java
@@ -0,0 +1,85 @@
+package interview.easy.phase2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author qcy
+ * @create 2021/03/21 22:15:39
+ * 面试题 02.01. 移除重复节点
+ *
+ * 编写代码，移除未排序链表中的重复节点。保留最开始出现的节点。
+ *
+ * 示例1:
+ *
+ *  输入：[1, 2, 3, 3, 2, 1]
+ *  输出：[1, 2, 3]
+ * 示例2:
+ *
+ *  输入：[1, 1, 1, 1, 2]
+ *  输出：[1, 2]
+ * 提示：
+ *
+ * 链表长度在[0, 20000]范围内。
+ * 链表元素在[0, 20000]范围内。
+ * 进阶：
+ *
+ * 如果不得使用临时缓冲区，该怎么解决？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicate-node-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Number02dot01 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode removeDuplicateNodes(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode cur = head;
+        Set<Integer> set = new HashSet<>();
+
+        while (cur != null && cur.next != null) {
+            set.add(cur.val);
+            if (set.contains(cur.next.val)) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(3);
+        ListNode n5 = new ListNode(2);
+        ListNode n6 = new ListNode(1);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+        n5.next = n6;
+
+        ListNode first = new Number02dot01().removeDuplicateNodes(n1);
+        while (first != null) {
+            System.out.println(first.val);
+            first = first.next;
+        }
+
+    }
+
+}
