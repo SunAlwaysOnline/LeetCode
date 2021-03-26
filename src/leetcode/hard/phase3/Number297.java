78b309e4a43fecfcc9a7687376213b2347095f5b
SunAlwaysOnline <767638734@qq.com>
2021-03-26 10:11:33 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()|notice()第297题

--- src/leetcode/hard/phase3/Number297.java
+++ src/leetcode/hard/phase3/Number297.java
@@ -2,7 +2,6 @@ package leetcode.hard.phase3;
 
 import datastructure.BinaryTree;
 import method.Classic;
-import method.InComplete;
 
 import java.util.LinkedList;
 import java.util.Queue;
@@ -10,10 +9,48 @@ import java.util.Queue;
 /**
  * @author qcy
  * @create 2020/12/24 14:44:31
+ * 297. 二叉树的序列化与反序列化
+ *
+ * 序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，
+ * 同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
+ *
+ * 请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，
+ * 你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
+ *
+ * 提示: 输入输出格式与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。
+ * 你并非必须采取这种方式，你也可以采用其他的方法解决这个问题。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [1,2,3,null,null,4,5]
+ * 输出：[1,2,3,null,null,4,5]
+ * 示例 2：
+ *
+ * 输入：root = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：root = [1]
+ * 输出：[1]
+ * 示例 4：
+ *
+ * 输入：root = [1,2]
+ * 输出：[1,2]
+ *  
+ *
+ * 提示：
+ *
+ * 树中结点数在范围 [0, 104] 内
+ * -1000 <= Node.val <= 1000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-@BinaryTree
 @Classic
-@InComplete
+@BinaryTree
 public class Number297 {
 
     public static class TreeNode {
@@ -32,40 +69,20 @@ public class Number297 {
             if (root == null) {
                 return "[]";
             }
-            StringBuilder sb = new StringBuilder();
-            sb.append("[");
 
-            int allNodeNum = (1 << getDepth(root)) - 1;
-            int countNodeNum = 0;
+            StringBuilder sb = new StringBuilder("[");
             Queue<TreeNode> queue = new LinkedList<>();
             queue.offer(root);
 
             while (!queue.isEmpty()) {
                 TreeNode node = queue.poll();
-                countNodeNum++;
-                if (countNodeNum > allNodeNum) {
-                    break;
-                }
                 if (node == null) {
                     sb.append("null").append(",");
-                    queue.offer(null);
-                    queue.offer(null);
-                } else {
-                    sb.append(node.val).append(",");
-                    queue.offer(node.left);
-                    queue.offer(node.right);
+                    continue;
                 }
-            }
-
-            while (true) {
-                if (sb.length() < 5) {
-                    break;
-                }
-                String s = sb.substring(sb.length() - 5, sb.length());
-                if (!s.equals("null,")) {
-                    break;
-                }
-                sb.delete(sb.length() - 5, sb.length());
+                sb.append(node.val).append(",");
+                queue.offer(node.left);
+                queue.offer(node.right);
             }
 
             sb.deleteCharAt(sb.length() - 1);
@@ -74,55 +91,32 @@ public class Number297 {
         }
 
         public TreeNode deserialize(String data) {
-            if (data == null || "".equals(data) || "[]".equals(data)) {
+            if (data.equals("[]")) {
                 return null;
             }
-            String[] split = data.split("\\[")[1].split("]")[0].split(",");
-            int allNodeSum = split.length;
-            int index = 0;
-            int curNodeSum = 0;
-
+            String[] array = data.substring(1, data.length() - 1).split(",");
+            TreeNode root = new TreeNode(Integer.parseInt(array[0]));
             Queue<TreeNode> queue = new LinkedList<>();
-            TreeNode root = new TreeNode(Integer.parseInt(split[index++]));
-            if (allNodeSum == 1) {
-                return root;
-            }
             queue.offer(root);
+            int i = 1;
 
             while (!queue.isEmpty()) {
-                curNodeSum++;
-                if (curNodeSum > allNodeSum) {
-                    break;
-                }
                 TreeNode node = queue.poll();
-                if (node == null) {
-                    continue;
+                if (!"null".equals(array[i])) {
+                    node.left = new TreeNode(Integer.parseInt(array[i]));
+                    queue.offer(node.left);
                 }
-                node.left = getNode(split[index++]);
-                node.right = getNode(split[index++]);
-                queue.offer(node.left);
-                queue.offer(node.right);
-                curNodeSum += 2;
+                i++;
+                if (!"null".equals(array[i])) {
+                    node.right = new TreeNode(Integer.parseInt(array[i]));
+                    queue.offer(node.right);
+                }
+                i++;
             }
+
             return root;
         }
 
-        private int getDepth(TreeNode node) {
-            if (node == null) {
-                return 0;
-            }
-            return Math.max(getDepth(node.left), getDepth(node.right)) + 1;
-        }
-
-        private TreeNode getNode(String num) {
-            TreeNode node = null;
-            try {
-                int val = Integer.parseInt(num);
-                node = new TreeNode(val);
-            } catch (NumberFormatException e) {
-            }
-            return node;
-        }
     }
 
     public static void main(String[] args) {
