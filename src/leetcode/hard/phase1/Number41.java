ef79e1ab68c7eec3c05f940bad7de0743bb2dbe7
SunAlwaysOnline <767638734@qq.com>
2021-03-26 16:56:02 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|sql()|notice()第41题

---
+++ src/leetcode/hard/phase1/Number41.java
@@ -0,0 +1,72 @@
+package leetcode.hard.phase1;
+
+import datastructure.Array;
+import method.Classic;
+import method.InPlace;
+
+/**
+ * @author qcy
+ * @create 2021/03/26 16:31:50
+ * 41. 缺失的第一个正数
+ *
+ * 给你一个未排序的整数数组 nums ，请你找出其中没有出现的最小的正整数。
+ *
+ * 进阶：你可以实现时间复杂度为 O(n) 并且只使用常数级别额外空间的解决方案吗？
+ * 
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,0]
+ * 输出：3
+ * 示例 2：
+ *
+ * 输入：nums = [3,4,-1,1]
+ * 输出：2
+ * 示例 3：
+ *
+ * 输入：nums = [7,8,9,11,12]
+ * 输出：1
+ *  
+ *
+ * 提示：
+ *
+ * 0 <= nums.length <= 300
+ * -231 <= nums[i] <= 231 - 1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/first-missing-positive
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Classic
+@Array
+public class Number41 {
+
+    @InPlace
+    public int firstMissingPositive(int[] nums) {
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            while (nums[i] >= 1 && nums[i] <= length && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return length + 1;
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Number41().firstMissingPositive(new int[]{3, 4, -1, 1}));
+    }
+
+}
