96d79544517b0ba9ac9ba0abde65f1b62058304d
SunAlwaysOnline <767638734@qq.com>
2021-04-03 20:28:26 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|notice()|sql()第221题

--- src/leetcode/medium/phase3/Number221.java
+++ src/leetcode/medium/phase3/Number221.java
@@ -2,29 +2,75 @@ package leetcode.medium.phase3;
 
 import datastructure.Grid;
 import method.Classic;
-import method.WaitForComplete;
+import method.DP;
 
 /**
  * @author qcy
  * @create 2021/02/27 18:17:48
+ * 221. 最大正方形
+ *
+ * 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * 输出：4
+ * 示例 2：
+ *
+ *
+ * 输入：matrix = [["0","1"],["1","0"]]
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：matrix = [["0"]]
+ * 输出：0
+ *  
+ *
+ * 提示：
+ *
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 300
+ * matrix[i][j] 为 '0' 或 '1'
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximal-square
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 @Classic
 @Grid
-@WaitForComplete
+@DP
 public class Number221 {
-
-    int max = 0;
 
     public int maximalSquare(char[][] matrix) {
         int row = matrix.length;
-        int col = matrix[0].length;
+        int col = 0;
+        if (row == 0 || (col = matrix[0].length) == 0) {
+            return 0;
+        }
 
-        for (int i = 0; i < row - 1; i++) {
-            for (int j = 0; j < col - 1; j++) {
-
+        int maxSide = 0;
+        int[][] dp = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == '0') {
+                    continue;
+                }
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = Math.min(dp[i][j - 1], Math.min(dp[i - 1][j], dp[i - 1][j - 1])) + 1;
+                }
+                maxSide = Math.max(maxSide, dp[i][j]);
             }
         }
-        return -1;
+        return maxSide * maxSide;
+    }
+
+    public static void main(String[] args) {
+
     }
 
 }
