9bf57f761909115223553455d698a39648246af8
SunAlwaysOnline <767638734@qq.com>
2021-04-04 12:08:23 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|notice()|sql()第189题

---
+++ src/leetcode/medium/phase2/Number189.java
@@ -0,0 +1,80 @@
+package leetcode.medium.phase2;
+
+import datastructure.Array;
+import method.Classic;
+import method.InPlace;
+
+import java.util.Arrays;
+
+/**
+ * @author qcy
+ * @create 2021/04/04 11:34:18
+ * 189. 旋转数组
+ *
+ * 给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+ * 
+ *
+ * 进阶：
+ *
+ * 尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+ * 你可以使用空间复杂度为 O(1) 的 原地 算法解决这个问题吗？
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [1,2,3,4,5,6,7], k = 3
+ * 输出: [5,6,7,1,2,3,4]
+ * 解释:
+ * 向右旋转 1 步: [7,1,2,3,4,5,6]
+ * 向右旋转 2 步: [6,7,1,2,3,4,5]
+ * 向右旋转 3 步: [5,6,7,1,2,3,4]
+ * 示例 2:
+ *
+ * 输入：nums = [-1,-100,3,99], k = 2
+ * 输出：[3,99,-1,-100]
+ * 解释:
+ * 向右旋转 1 步: [99,-1,-100,3]
+ * 向右旋转 2 步: [3,99,-1,-100]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 2 * 104
+ * -231 <= nums[i] <= 231 - 1
+ * 0 <= k <= 105
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotate-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Classic
+@Array
+public class Number189 {
+
+    @InPlace
+    public void rotate(int[] nums, int k) {
+        int length = nums.length;
+        k %= length;
+
+        reverse(nums, 0, length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        new Number189().rotate(nums, 3);
+        System.out.println(Arrays.toString(nums));
+    }
+
+}
