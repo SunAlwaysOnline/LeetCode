d51a14a7bc44e79805cdb168f349295a4aa6991d
SunAlwaysOnline <767638734@qq.com>
2021-04-04 13:44:34 +0800

    add:提交leetcode()|jzoffer(√)|interview()|lcp()|notice()|sql()第48题

---
+++ src/jzoffer/medium/phase1/Number48.java
@@ -0,0 +1,67 @@
+package jzoffer.medium.phase1;
+
+import method.Classic;
+import method.StringOperation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author qcy
+ * @create 2021/04/04 13:10:08
+ * 剑指 Offer 48. 最长不含重复字符的子字符串
+ *
+ * 请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
+ * 
+ *
+ * 示例 1:
+ *
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ *
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ *
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ *  
+ *
+ * 提示：
+ *
+ * s.length <= 40000
+ * 注意：本题与主站 3 题相同：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zui-chang-bu-han-zhong-fu-zi-fu-de-zi-zi-fu-chuan-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Classic
+@StringOperation
+public class Number48 {
+
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int maxLength = 0;
+        int start = 0;
+        int end = 0;
+
+        for (; end < s.length(); end++) {
+            Integer endCharIndex = map.get(s.charAt(end));
+            if (endCharIndex != null) {
+                start = Math.max(start, endCharIndex + 1);
+            }
+            map.put(s.charAt(end), end);
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+
+        return maxLength;
+    }
+
+
+}
