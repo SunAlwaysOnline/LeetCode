c3169ba8edd14d8e8e24775057bc3a00439c0457
SunAlwaysOnline <767638734@qq.com>
2021-04-05 22:32:15 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|notice()|sql()第309题

--- src/leetcode/medium/phase4/Number309.java
+++ src/leetcode/medium/phase4/Number309.java
@@ -2,19 +2,56 @@ package leetcode.medium.phase4;
 
 import datastructure.Array;
 import method.Classic;
+import method.DP;
 
 /**
  * @author qcy
  * @create 2021/03/01 13:50:11
+ * 309. 最佳买卖股票时机含冷冻期
+ *
+ * 给定一个整数数组，其中第 i 个元素代表了第 i 天的股票价格 。​
+ *
+ * 设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
+ *
+ * 你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * 卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
+ * 示例:
+ *
+ * 输入: [1,2,3,0,2]
+ * 输出: 3
+ * 解释: 对应的交易状态为: [买入, 卖出, 冷冻期, 买入, 卖出]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-cooldown
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 @Classic
 @Array
 public class Number309 {
 
+
+    @DP
     public int maxProfit(int[] prices) {
+        int length = prices.length;
+        if (length <= 1) {
+            return 0;
+        }
 
+        int[][] dp = new int[length][3];
+        //dp[i][0]:第i天不持有且没卖出能获得的最大收益
+        //dp[i][1]:第i天持有能获得的最大收益
+        //dp[i][2]:第i天不持有且卖出能获得的最大收益
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        dp[0][2] = 0;
 
-        return -1;
+        for (int i = 1; i < length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            dp[i][2] = dp[i - 1][1] + prices[i];
+        }
+
+        return Math.max(dp[length - 1][0], dp[length - 1][2]);
     }
 
 }
