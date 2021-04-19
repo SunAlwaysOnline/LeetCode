fa5587f9fdd5e2381f29b7f8b7f523a4bd0319f7
SunAlwaysOnline <767638734@qq.com>
2021-04-19 21:07:48 +0800

    add:提交leetcode(√)|jzoffer()|interview()|lcp()|notice()|sql()第707题

---
+++ src/leetcode/medium/phase8/Number707.java
@@ -0,0 +1,133 @@
+package leetcode.medium.phase8;
+
+import datastructure.LinkedList;
+import method.Classic;
+import method.Design;
+
+/**
+ * @author qcy
+ * @create 2021/04/19 20:45:17
+ * 707. 设计链表
+ *
+ * 设计链表的实现。您可以选择使用单链表或双链表。单链表中的节点应该具有两个属性：val 和 next。
+ * val 是当前节点的值，next 是指向下一个节点的指针/引用。如果要使用双向链表，则还需要一个属性 prev 以指示链表中的上一个节点。
+ * 假设链表中的所有节点都是 0-index 的。
+ *
+ * 在链表类中实现这些功能：
+ *
+ * get(index)：获取链表中第 index 个节点的值。如果索引无效，则返回-1。
+ * addAtHead(val)：在链表的第一个元素之前添加一个值为 val 的节点。插入后，新节点将成为链表的第一个节点。
+ * addAtTail(val)：将值为 val 的节点追加到链表的最后一个元素。
+ * addAtIndex(index,val)：在链表中的第 index 个节点之前添加值为 val  的节点。
+ * 如果 index 等于链表的长度，则该节点将附加到链表的末尾。如果 index 大于链表长度，则不会插入节点。
+ * 如果index小于0，则在头部插入节点。
+ * deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 index 个节点。
+ *  
+ *
+ * 示例：
+ *
+ * MyLinkedList linkedList = new MyLinkedList();
+ * linkedList.addAtHead(1);
+ * linkedList.addAtTail(3);
+ * linkedList.addAtIndex(1,2);   //链表变为1-> 2-> 3
+ * linkedList.get(1);            //返回2
+ * linkedList.deleteAtIndex(1);  //现在链表是1-> 3
+ * linkedList.get(1);            //返回3
+ *  
+ *
+ * 提示：
+ *
+ * 所有val值都在 [1, 1000] 之内。
+ * 操作次数将在  [1, 1000] 之内。
+ * 请不要使用内置的 LinkedList 库。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/design-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@Classic
+@Design
+@LinkedList
+public class Number707 {
+
+    class MyLinkedList {
+        Node head;
+        int size;
+
+        public MyLinkedList() {
+            head = new Node(-1);
+            size = 0;
+        }
+
+        public int get(int index) {
+            if (index >= size) {
+                return -1;
+            }
+            Node cur = head;
+            index += 1;
+            while (index != 0) {
+                cur = cur.next;
+                index--;
+            }
+            return cur.val;
+        }
+
+        public void addAtHead(int val) {
+            Node headNext = head.next;
+            Node newNode = new Node(val);
+            head.next = newNode;
+            newNode.next = headNext;
+            size++;
+        }
+
+        public void addAtTail(int val) {
+            Node cur = head;
+            while (cur != null && cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = new Node(val);
+            size++;
+        }
+
+        public void addAtIndex(int index, int val) {
+            if (index <= 0) {
+                addAtHead(val);
+                return;
+            }
+            Node cur = head;
+            while (index != 0) {
+                cur = cur.next;
+                index--;
+            }
+            Node nextNode = cur.next;
+            Node newNode = new Node(val);
+            cur.next = newNode;
+            newNode.next = nextNode;
+            size++;
+        }
+
+        public void deleteAtIndex(int index) {
+            if (index >= size) {
+                return;
+            }
+            Node cur = head;
+            while (index != 0) {
+                cur = cur.next;
+                index--;
+            }
+            Node nextNextNode = cur.next.next;
+            cur.next = nextNextNode;
+            size--;
+        }
+    }
+
+    class Node {
+        int val;
+        Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+}
