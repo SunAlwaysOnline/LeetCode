061e0b22bb90dc1aef4c999c4968720aee2536dd
SunAlwaysOnline <767638734@qq.com>
2021-05-10 10:15:11 +0800

    review:反转链表

---
+++ src/important/linkedList/Reverse.java
@@ -0,0 +1,71 @@
+package important.linkedList;
+
+import method.Iteration;
+import method.Recursion;
+
+/**
+ * @author qcy
+ * @create 2021/05/10 09:50:16
+ * 反转链表
+ */
+public class Reverse {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    @Iteration
+    public ListNode reverseList1(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+    }
+
+    @Recursion
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        ListNode res = new Reverse().reverseList(n1);
+    }
+
+}
---
+++ src/leetcode/easy/phase3/Number206.java
@@ -0,0 +1,102 @@
+package leetcode.easy.phase3;
+
+import method.Iteration;
+import method.Recursion;
+
+/**
+ * @author qcy
+ * @create 2021/05/10 09:53:53
+ * 206. 反转链表
+ *
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * 示例 2：
+ *
+ *
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ * 示例 3：
+ *
+ * 输入：head = []
+ * 输出：[]
+ *  
+ *
+ * 提示：
+ *
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 <= Node.val <= 5000
+ *  
+ *
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Number206 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    @Iteration
+    public ListNode reverseList1(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+    }
+
+    @Recursion
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        ListNode res = new Number206().reverseList(n1);
+    }
+
+}
