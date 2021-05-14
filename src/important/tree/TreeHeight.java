d2a1865e1941a1a28608847e3922db90ed9e8ea0
SunAlwaysOnline <767638734@qq.com>
2021-05-14 14:30:52 +0800

    review:二叉树的高度

---
+++ src/important/tree/TreeHeight.java
@@ -0,0 +1,41 @@
+package important.tree;
+
+/**
+ * @author qcy
+ * @create 2021/05/14 14:06:10
+ * 二叉树的高度
+ */
+public class TreeHeight {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //通过自下而上的递归，或者通过层序遍历的方式
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+}
