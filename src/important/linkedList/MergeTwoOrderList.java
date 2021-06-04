711d6e98b7b5f9e6e80741c303654e8171d14a56
SunAlwaysOnline <767638734@qq.com>
2021-06-04 11:29:15 +0800

    review:合并两个有序链表

---
+++ src/important/linkedList/MergeTwoOrderList.java
@@ -0,0 +1,63 @@
+package important.linkedList;
+
+/**
+ * @author qcy
+ * @create 2021/06/04 11:21:20
+ * 合并两个有序链表
+ */
+public class MergeTwoOrderList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        ListNode virtualNode = new ListNode();
+        ListNode cur = virtualNode;
+
+        while (l1 != null || l2 != null) {
+            if (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    cur.next = l1;
+                    l1 = l1.next;
+                } else {
+                    cur.next = l2;
+                    l2 = l2.next;
+                }
+                cur = cur.next;
+                continue;
+            }
+            if (l1 == null) {
+                cur.next = l2;
+                l2 = l2.next;
+            } else {
+                cur.next = l1;
+                l1 = l1.next;
+            }
+            cur = cur.next;
+        }
+
+        return virtualNode.next;
+    }
+}
