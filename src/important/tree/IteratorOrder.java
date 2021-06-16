a3011a871de576e1d63507190af6a8b33538de50
SunAlwaysOnline <767638734@qq.com>
2021-06-16 16:02:35 +0800

    review:非递归遍历二叉树

---
+++ src/important/tree/IteratorOrder.java
@@ -0,0 +1,122 @@
+package important.tree;
+
+import util.ListUtil;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author qcy
+ * @create 2021/06/16 15:43:16
+ * 非递归遍历二叉树
+ */
+public class IteratorOrder {
+
+    //前序遍历
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                list.add(root.val);
+                stack.push(root);
+                root = root.left;
+            } else {
+                TreeNode node = stack.pop();
+                root = node.right;
+            }
+        }
+
+        return list;
+    }
+
+    //中序遍历
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                TreeNode node = stack.pop();
+                list.add(node.val);
+                root = node.right;
+            }
+        }
+
+        return list;
+    }
+
+    //后序遍历
+    //即对(右、中、左的翻转)
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                list.add(root.val);
+                stack.push(root);
+                root = root.right;
+            } else {
+                TreeNode node = stack.pop();
+                root = node.left;
+            }
+        }
+        Collections.reverse(list);
+
+        return list;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n7 = new TreeNode(7);
+
+        n1.left = n2;
+        n1.right = n3;
+
+        n2.left = n4;
+        n2.right = n5;
+
+        n3.left = n6;
+        n3.right = n7;
+
+        //       1
+        //     /   \
+        //    2     3
+        //  /  \   / \
+        // 4   5  6   7
+
+        List<Integer> list = new IteratorOrder().postorderTraversal(n1);
+        ListUtil.print(list);
+    }
+}
